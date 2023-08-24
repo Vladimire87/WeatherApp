511479154f532fb0cb53dc0e3689cb24e457706c
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-24 22:05:26 +0400

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,19 @@ class HomeController < ApplicationController
     # @real_ip = ['76.218.85.81'] if @real_ip.nil?
     # response = Faraday.get("https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=#{@real_ip[0]}")
     # response_hash = JSON.parse(response.body)
-    # city = response_hash['city']
     @city = request.location.city if Rails.env.production?
+
+    city = @city
 
     client = OpenWeather::Client.new(
       api_key: 'bc1805a2fb4fe6801980703de9b4f44f'
     )
-    @data = client.current_weather(@city)
+    @data = client.current_weather(city:)
+    # if Rails.env.production?
+    #   @country = request.location.country_code
+    #   @city = request.location.city
+    #   @currency = @country.upcase == "AU" ? "AU$" : "$"
+    # end
   end
 
   def show; end
