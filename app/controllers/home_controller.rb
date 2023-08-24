d02adb12605e12861dab411679fefbe97469ffd2
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-24 19:18:08 +0400

    upd controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,8 @@ class HomeController < ApplicationController
   def index
     # user_ip = request.remote_ip
     @real_ip = request.env['HTTP_X_FORWARDED_FOR']
-    @real_ip1 = request.remote_ip
 
-    @real_ip2 = request.env['HTTP_X_FORWARDED_FOR']
-
-    @real_ip3 = request.remote_addr
-
-    @real_ip4 = request.env['REMOTE_ADDR']
-
-    response = Faraday.get("https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=#{@real_ip1}")
+    response = Faraday.get("https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=#{@real_ip}")
 
     response_hash = JSON.parse(response.body)
     city = response_hash['city']
@@ -21,7 +14,6 @@ class HomeController < ApplicationController
       api_key: 'bc1805a2fb4fe6801980703de9b4f44f'
     )
     @data = client.current_weather(city:)
-    raise
   end
 
   def show; end
