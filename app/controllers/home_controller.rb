1d62c893a8cc30063aedc3798f824c6642d65154
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-24 19:11:14 +0400

    add ip to test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,16 @@
 class HomeController < ApplicationController
   def index
     # user_ip = request.remote_ip
-    real_ip = request.env['HTTP_X_FORWARDED_FOR']
+    @real_ip = request.env['HTTP_X_FORWARDED_FOR']
+    @real_ip1 = request.remote_ip
 
-    response = Faraday.get("https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=#{real_ip}")
+    @real_ip2 = request.env['HTTP_X_FORWARDED_FOR']
+
+    @real_ip3 = request.remote_addr
+
+    @real_ip4 = request.env['REMOTE_ADDR']
+
+    response = Faraday.get("https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=#{@real_ip1}")
 
     response_hash = JSON.parse(response.body)
     city = response_hash['city']
