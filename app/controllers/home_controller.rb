36544de7a4bca6d63da2a55c71f292091a3aed1d
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-24 19:29:28 +0400

    get visitor ip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     # user_ip = request.remote_ip
     @real_ip = request.env['HTTP_X_FORWARDED_FOR'].split(',')
 
-    response = Faraday.get('https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=212.58.103.63')
+    response = Faraday.get("https://api.ipgeolocation.io/ipgeo?apiKey=943115cc753b424aa65feec8e4c2b673&ip=#{@real_ip[0]}")
 
     response_hash = JSON.parse(response.body)
     city = response_hash['city']
