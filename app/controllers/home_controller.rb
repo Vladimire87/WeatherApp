ebdd154623c9f020255d2d5d56f2bc999b37c1ea
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-29 15:34:25 +0400

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,26 @@
 
 class HomeController < ApplicationController
   def index
-    @city = request.location.city if Rails.env.production?
-    @city = 'Moscow' if Rails.env.development?
-
-    city = @city
-
-    client = OpenWeather::Client.new(
-      api_key: 'bc1805a2fb4fe6801980703de9b4f44f'
-    )
-    $data = client.current_weather(city:)
+    @city = current_city
+    fetch_weather_data(@city)
   end
 
   def show; end
+
+  private
+
+  def current_city
+    if Rails.env.production?
+      request.location.city
+    else
+      'Moscow'
+    end
+  end
+
+  def fetch_weather_data(city)
+    client = OpenWeather::Client.new(
+      api_key: ENV.fetch('OPENWEATHER_API_KEY', nil)
+    )
+    client.current_weather(city:)
+  end
 end
